f1a21c772e0cc53761fa3ac1ec876357ba875552
Mushan khan <121488978+Khanba22@users.noreply.github.com>
2024-11-11 18:32:44 +0530

    cleaned code

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { csvToJson } = require("./functions/csvToJsonConverter");
-const { generateDataFrame } = require("./functions/generateDataFrame");
 const connectToMongo = require("./Database/DB");
 dotenv.config();
 connectToMongo()
@@ -15,31 +12,6 @@ app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-
-// POST route to handle CSV file upload and conversion
-app.post("/api/convert-csv", upload.single("file"), async (req, res) => {
-  try {
-    // Check if file is uploaded
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
-
-    // Call csvToJson function with the uploaded file path
-    const jsonResult = await csvToJson(req.file.path);
-    const jsonDataFrame = generateDataFrame(jsonResult);
-    const rowDf = jsonDataFrame.getRows();
-
-    // Delete the file after processing
-    fs.unlinkSync(req.file.path);
-
-    // Send JSON result as response
-    res.json({ dataFrame: rowDf, keys: jsonDataFrame.keys });
-  } catch (error) {
-    console.error("Error in CSV conversion:", error);
-    res.status(500).json({ error: "Failed to convert CSV to JSON" });
-  }
-});
-
 // Use design route for template handling
 app.use("/api/design", require("./routes/templateRoute"));
 app.use("/api/certificate", require("./routes/certificateRoute"));
